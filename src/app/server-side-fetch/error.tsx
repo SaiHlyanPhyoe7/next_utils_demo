114769f45180f8eaed2b1bd0fb63bb36f512c788
SaiHlyanPhyoe <saihlyan.phyoe@kbzbank.com>
2023-06-07 16:33:52 +0630

    feat:add error handling page

---
+++ src/app/server-side-fetch/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const error = () => {
+	return (
+		<div>
+			<h1 className="text-red-400">This is error page</h1>
+		</div>
+	);
+};
+
+export default error;
--- src/app/server-side-fetch/page.tsx
+++ src/app/server-side-fetch/page.tsx
@@ -2,14 +2,20 @@
 
 import React, { Suspense } from "react";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { getData } from "./prefetch";
 import Loading from "./loading";
 import Link from "next/link";
+interface PostType {
+	post: {
+		title: string;
+		body: string;
+	};
+}
+
+const session = null;
 
 export default function Page() {
-	// const data = await getData();
-
+	// if (!session) throw new Error("Error is happening.");
 	const { isLoading, error, data, isFetching } = useQuery({
 		queryKey: ["todoData"],
 		queryFn: () => getData(),
@@ -17,22 +23,23 @@ export default function Page() {
 
 	if (isLoading || !data) return "Loading...";
 
-	// if (error) return "An error has occurred: " + error;
-
 	return (
 		<Suspense fallback={<Loading />}>
 			<Link href="/server-action">Goto Server Action</Link>
-			{/* {data} */}
 			<div>
 				{data?.map((post: any) => {
-					return (
-						<div key={post.id}>
-							{post.title}
-							{post.body}
-						</div>
-					);
+					return <Posts key={post.id} post={post}></Posts>;
 				})}
 			</div>
 		</Suspense>
+	);
+}
+
+function Posts({ post }: PostType) {
+	return (
+		<div>
+			{post.title}
+			{post.body}
+		</div>
 	);
 }
