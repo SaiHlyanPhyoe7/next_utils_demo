2335ac28ff899682cee3027d4d53612d04100c05
SaiHlyanPhyoe <saihlyan.phyoe@kbzbank.com>
2023-06-16 10:34:16 +0630

    feat:server fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function Home() {
+interface PostType {
+	title: string;
+	body: string;
+}
+
+async function getData() {
+	const res = await axios
+		.get("https://jsonplaceholder.typicode.com/posts?_page=0&_limit=10")
+		.then((res) => res.data);
+	// console.log("Result Get Data is :", res);
+	return res;
+}
+
+export default async function Home() {
+	const serverData = await getData();
+	console.log("Server Data is :", serverData);
 	return (
 		<div className="flex justify-center items-center h-screen gap-4">
 			{numbers.map((num) => {
@@ -12,6 +28,9 @@ export default function Home() {
 						className="bg-indigo-400 rounded-md text-white px-4 py-2"
 						key={num}
 					>
+						{serverData.map((post: PostType) => {
+							return <div key={post.title}>{post.body}</div>;
+						})}
 						<Link href={`numbs/${num}`}>{num}</Link>
 					</div>
 				);
@@ -19,4 +38,3 @@ export default function Home() {
 		</div>
 	);
 }
-
--- src/app/server-side-fetch/prefetch.ts
+++ src/app/server-side-fetch/prefetch.ts
@@ -6,6 +6,6 @@ export async function getData() {
 	const res = await axios
 		.get("https://jsonplaceholder.typicode.com/posts?_page=0&_limit=10")
 		.then((res) => res.data);
-	console.log("Result Get Data is :", res);
+	// console.log("Result Get Data is :", res);
 	return res;
 }
